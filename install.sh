bee2e3ddbe240e6f75b1556310aefa1fdc0462eb
Anton Stasyuk <antonstsk@gmail.com>
2020-05-27 10:32:54 +0200

    added a todo

--- install.sh
+++ install.sh
@@ -6,13 +6,15 @@ read -p "This may overwrite existing files in home directory. Are you sure? (y/n
 echo "";
 
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-	
+
 	rsync --exclude ".git/" \
 		--exclude ".DS_Store" \
 		--exclude "install.sh" \
 		--exclude "brew.sh" \
 		--exclude "README.md" \
 		-avh --no-perms . ~;
-	
+
 	source ~/.bash_profile;
-fi; 
+fi;
+
+# todo: ask for git[user] and git[email] >> ~/.gitconfig
